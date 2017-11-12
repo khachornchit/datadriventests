37be102071673f085161d3560f951be0dc564391
Kajornjit Songsaen <kajornjit@gmail.com>
2017-11-12 15:22:04 +0700

    first commit

---
+++ DataSourceTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data.Odbc;
+
+namespace PlutoSolutionsDataDrivenUnitTests
+{
+    [TestClass]
+    public class DataSourceTest
+    {
+        public TestContext context { get; set; }
+
+        [TestMethod]
+        //[DeploymentItem("PlutoSolutionsDataDrivenUnitTests\\DBConnection.xlsx")]
+        //[DataSource("MyExcelDataSource")]
+        [DataSource(
+            "System.Data.OleDb",
+            "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=DBConnection.xls",
+            "Sheet1$",
+            DataAccessMethod.Sequential
+            )]
+        public void ReadDataTest()
+        {
+            int A = int.Parse(context.DataRow["A"].ToString());
+            int B = int.Parse(context.DataRow["B"].ToString());
+            int expected = int.Parse(context.DataRow["C"].ToString());
+            int actual = MathTest.Add(A, B);
+
+            Console.WriteLine(string.Format("expected / actual : ", expected, actual));
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ MathTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlutoSolutionsDataDrivenUnitTests
+{
+    public class MathTest
+    {
+        public static int Add(int numberA, int numberB)
+        {
+            return numberA + numberB;
+        }
+    }
+}
